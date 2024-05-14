85a18977f7fc60a38d3547b2457b5c5df6466d5a
Andy Wilkinson <andy.wilkinson@broadcom.com>
2024-05-14 14:11:48 +0100

    Initial commit

---
+++ src/main/java/com/example/springboot40760/SpringBoot40760Application.java
@@ -0,0 +1,13 @@
+package com.example.springboot40760;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot40760Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot40760Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springboot40760/SpringBoot40760ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot40760;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot40760ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
